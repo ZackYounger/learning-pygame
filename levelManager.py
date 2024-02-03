844792d4d09f3177d7be5708f7ea4821a6be29c4
itsnotdanial <gkzv63@durham.ac.uk>
2024-02-03 20:32:04 +0000

    Added some comments

--- levelManager.py
+++ levelManager.py
@@ -1,3 +1,20 @@
+""" 
+    Take the file and read it.
+    determine whicg goes where 
+    Read through it as an Array 
+    If it's a 1 then place a sprite 
+    If 0 place something else.
+ 
+ """
+
 class levelManager:
     def __init__(self):
         pass
+
+    def TestList():
+        List = [[0, 1, 0], [1, 0, 1]]
+        for i in List :
+            for j in i :
+                if j == 0 :
+                    
+        return 
