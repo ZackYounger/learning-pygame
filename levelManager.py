6ec19bc9e0ad891c3feccc40b2b33824ebf98f97
itsnotdanial <gkzv63@durham.ac.uk>
2024-03-03 17:35:53 +0000

    done levelManager sort of

--- levelManager.py
+++ levelManager.py
@@ -1,20 +1,38 @@
-""" 
-    Take the file and read it.
-    determine whicg goes where 
-    Read through it as an Array 
-    If it's a 1 then place a sprite 
-    If 0 place something else.
- 
- """
+import csv
+import pygame 
 
-class levelManager:
-    def __init__(self):
+class levelManager :
+    def __init__(self) :
         pass
 
-    def TestList():
-        List = [[0, 1, 0], [1, 0, 1]]
-        for i in List :
-            for j in i :
-                if j == 0 :
-                    
-        return 
+    
+    def convertingToList(self, filename) : 
+        self.level_data = []
+        with open( filename, "r") as csvfile:
+            csv_data = csv.reader(csvfile, delimiter=",")
+            for row in csv_data:
+                self.level_data.append(row)
+
+               
+
+    
+    def drawing(self, screen) :
+        #Reading the height(i) and the width(j)
+        #Provide x and y where to draw 
+        
+        for i in range(len(self.level_data)) :
+            for j in range(len(self.level_data)) :
+                if self.level_data[i][j] == 0 :
+                    pygame.draw.rect(screen, (255, 255, 255) [i, j, 20, 20])
+
+    
+
+                
+
+
+
+
+
+
+
+
